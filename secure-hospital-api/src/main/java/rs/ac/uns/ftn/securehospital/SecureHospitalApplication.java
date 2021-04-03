8991baa0451125cf6f8b34464a0abd816315a3ef
Luka Banjac <banjacluka96@gmail.com>
2021-04-03 16:43:56 +0200

    init

---
+++ secure-hospital-api/src/main/java/rs/ac/uns/ftn/securehospital/SecureHospitalApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.securehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureHospitalApplication.class, args);
+	}
+
+}
---
+++ secure-hospital-api/src/test/java/rs/ac/uns/ftn/securehospital/SecureHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.securehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
