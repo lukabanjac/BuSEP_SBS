a2eda4d89d46bbfd768d8333e9e989bfa5652a3f
Luka Banjac <banjacluka96@gmail.com>
2021-04-04 20:15:03 +0200

    administrator app

---
+++ administrator-app-api/src/test/java/rs/ac/uns/ftn/administratorappapi/AdministratorAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.administratorappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
